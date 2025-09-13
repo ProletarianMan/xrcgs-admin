c8d43f0a5844a1a2139e8bb2341057415e30c04c
ProletarianMan <92148429+ProletarianMan@users.noreply.github.com>
2025-09-13 22:07:45 +0800

    fix(iam): correct sys_menu timestamp fields

--- xrcgs-module-iam/src/main/java/com/xrcgs/iam/model/vo/MenuMetaVO.java
+++ xrcgs-module-iam/src/main/java/com/xrcgs/iam/model/vo/MenuMetaVO.java
@@ -7,9 +7,9 @@ import lombok.Data;
  */
 @Data
 public class MenuMetaVO {
-    private String title;
-    private String icon;
-    private Integer rank;
-    private Boolean keepAlive;
-    private Boolean showParent;
+    private String title;        // 菜单标题
+    private String icon;         // 图标
+    private Integer rank;        // 排序
+    private Boolean keepAlive;   // 组件是否缓存
+    private Boolean showParent;  // 是否显示父级
 }
