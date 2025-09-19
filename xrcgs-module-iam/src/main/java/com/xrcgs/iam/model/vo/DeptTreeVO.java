f73f2bfb5a1310186ae7418169e1b88ec1484abb
jacktangrui <jacktangdream@foxmail.com>
2025-09-20 01:52:38 +0800

    返回时间加上解析注解

--- xrcgs-module-iam/src/main/java/com/xrcgs/iam/model/vo/DeptTreeVO.java
+++ xrcgs-module-iam/src/main/java/com/xrcgs/iam/model/vo/DeptTreeVO.java
@@ -1,5 +1,6 @@
 package com.xrcgs.iam.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -22,6 +23,7 @@ public class DeptTreeVO {
     private String phone;
     private String email;
     private String remark;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
     private final List<DeptTreeVO> children = new ArrayList<>();
 }
