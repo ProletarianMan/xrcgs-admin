772f952f91906bcc7d4848169ead0e985e558d1e
jacktangrui <jacktangdream@foxmail.com>
2025-09-27 23:03:07 +0800

    用户管理权限综合简化

--- xrcgs-module-iam/src/main/java/com/xrcgs/iam/model/dto/UserUpsertDTO.java
+++ xrcgs-module-iam/src/main/java/com/xrcgs/iam/model/dto/UserUpsertDTO.java
@@ -3,7 +3,6 @@ package com.xrcgs.iam.model.dto;
 import com.xrcgs.iam.enums.DataScope;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
