32bac652b977cb391b90d80be6e18571b1decdea
ProletarianMan <92148429+ProletarianMan@users.noreply.github.com>
2025-10-08 12:42:00 +0800

    fix: mark exporter constructor for autowiring (#74)

--- xrcgs-module-road-safety/src/main/java/com/xrcgs/roadsafety/inspection/application/service/InspectionRecordExcelExporter.java
+++ xrcgs-module-road-safety/src/main/java/com/xrcgs/roadsafety/inspection/application/service/InspectionRecordExcelExporter.java
@@ -41,6 +41,7 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -76,6 +77,7 @@ public class InspectionRecordExcelExporter {
     private final Resource templateResourceOverride;
     private final InspectionLogProperties logProperties;
 
+    @Autowired
     public InspectionRecordExcelExporter(InspectionLogProperties logProperties) {
         this(logProperties, null);
     }
